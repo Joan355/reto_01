6b597c0fd72682cf286cca68f0c417a5d4e0025c
joan <a.salazar19992024@gmail.com>
2024-03-27 00:29:52 -0500

    Implementacion de procesos en el algoritmo, sin solucion de tiempo

--- jacobi1d.c
+++ jacobi1d.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <sys/shm.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <math.h>
+#include <sys/wait.h>
 #include "timing.h"
-
+#define CORES (int)(sysconf(_SC_NPROCESSORS_CONF) / 2)
 /* --
  * Do nsweeps sweeps of Jacobi iteration on a 1D Poisson problem
  * 
@@ -13,8 +17,10 @@
  * u is subject to Dirichlet boundary conditions specified in
  * the u[0] and u[n] entries of the initial vector.
  */
-void jacobi(int nsweeps, int n, double* u, double* f)
+void jacobi(int nsweeps, int n, double* u, double* f, int start, int end)
 {
+    //printf("start -> %i end -> %i\n", start, end);
+    //exit(0);
     int i, sweep;
     double h  = 1.0 / n;
     double h2 = h*h;
@@ -27,11 +33,11 @@ void jacobi(int nsweeps, int n, double* u, double* f)
     for (sweep = 0; sweep < nsweeps; sweep += 2) {
         
         /* Old data in u; new data in utmp */
-        for (i = 1; i < n; ++i)
+        for (i = start + 1; i < end + 1; ++i)
             utmp[i] = (u[i-1] + u[i+1] + h2*f[i])/2;
         
         /* Old data in utmp; new data in u */
-        for (i = 1; i < n; ++i)
+        for (i = start + 1; i < end + 1; ++i)
             u[i] = (utmp[i-1] + utmp[i+1] + h2*f[i])/2;
     }
 
@@ -66,16 +72,60 @@ int main(int argc, char** argv)
     fname  = (argc > 3) ? argv[3] : NULL;
     h      = 1.0/n;
 
+
+    //Crear memoria compartida
+    int shm_id_u = shmget(IPC_PRIVATE, (n + 1) * sizeof(double), IPC_CREAT | 0666);
+    if(shm_id_u < 0){
+        perror("Error en shmget");
+        return 1;
+    }
+
+
     /* Allocate and initialize arrays */
-    u = (double*) malloc( (n+1) * sizeof(double) );
+    //u = (double*) malloc( (n+1) * sizeof(double) );
+    u = (double *)shmat(shm_id_u, NULL, 0);
     f = (double*) malloc( (n+1) * sizeof(double) );
     memset(u, 0, (n+1) * sizeof(double));
     for (i = 0; i <= n; ++i)
         f[i] = i * h;
 
+
+    //Calculando segementos de vector
+    //Todo: Aqui se calcula los segementos que se repartiran entre los diferentes procesos
+    int p_r = (int)floor(n / CORES);
+    int step = p_r == 0 ? 1 : p_r;
+    int r_step = n % CORES;
+    int lim = (p_r == 0) ? r_step : CORES;
+
+
     /* Run the solver */
+    //Obtener tiempo de inicio de proceso
     get_time(&tstart);
-    jacobi(nsteps, n, u, f);
+
+    //calculo de jacobiano
+
+    pid_t pid = 1;
+
+    for(int i = 0; i < CORES; i++){
+        if(pid){
+            pid = fork();
+        }else if(pid < 0){
+            return 1;
+        }
+
+        if(pid == 0){
+            jacobi(nsteps, n, u, f,
+            i*step,
+            ((CORES - 1) == i) ? (i*step + r_step + step): (i*step + step));
+            exit(0);
+        }
+
+    }
+
+    for(int i = 0; i < CORES; i++){
+        wait(NULL);
+    }
+    //Obtener tiempo de finalizacion de proceso
     get_time(&tend);
 
     /* Run the solver */    
@@ -89,6 +139,7 @@ int main(int argc, char** argv)
         write_solution(n, u, fname);
 
     free(f);
-    free(u);
+    shmdt(u);
+    shmctl(shm_id_u, IPC_RMID, NULL);
     return 0;
 }
